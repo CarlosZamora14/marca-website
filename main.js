e538307d0aca09db89656db9f184f162594046c8
Carlos Zamora <carlos14zamora@gmail.com>
2023-09-09 22:50:58 -0600

    add page footer and fix carousel bug

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 // Carousel
 const carousel = document.querySelector('.custom-carousel');
+const carouselWidth = carousel.scrollWidth;
 const leftBtn = document.querySelector('.custom-carousel__left-btn');
 const rightBtn = document.querySelector('.custom-carousel__right-btn');
 const link = document.querySelector('.custom-carousel__show-more');
 
 function updateControls(currScrollLeft) {
   currScrollLeft === 0 ? leftBtn.classList.add('d-none') : leftBtn.classList.remove('d-none');
-  currScrollLeft + carousel.clientWidth >= carousel.scrollWidth ? rightBtn.classList.add('d-none') : rightBtn.classList.remove('d-none');
+  Math.round(currScrollLeft + carousel.clientWidth) >= carouselWidth ? rightBtn.classList.add('d-none') : rightBtn.classList.remove('d-none');
 
   leftBtn.style.left = `${currScrollLeft}px`;
   rightBtn.style.right = `-${currScrollLeft}px`;
   link.style.right = `-${currScrollLeft}px`;
 }
 
+function clampScrollLeft(scrollLeft) {
+  const currScrollLeft = Math.min(scrollLeft, carouselWidth - carousel.clientWidth);
+  return currScrollLeft;
+}
+
 window.addEventListener('resize', () => {
-  updateControls(carousel.scrollLeft);
+  const currScrollLeft = clampScrollLeft(carousel.scrollLeft);
+  updateControls(currScrollLeft);
 });
 
 carousel.addEventListener('scroll', () => {
-  updateControls(carousel.scrollLeft);
+  const currScrollLeft = clampScrollLeft(carousel.scrollLeft);
+  updateControls(currScrollLeft);
 });
 
 leftBtn.addEventListener('click', () => {
@@ -28,7 +36,7 @@ leftBtn.addEventListener('click', () => {
 });
 
 rightBtn.addEventListener('click', () => {
-  const currScrollLeft = Math.min(carousel.scrollLeft + carousel.clientWidth, carousel.scrollWidth - carousel.clientWidth);
+  const currScrollLeft = clampScrollLeft(carousel.scrollLeft + carousel.clientWidth);
   carousel.scrollLeft = currScrollLeft;
   updateControls(currScrollLeft);
 });
