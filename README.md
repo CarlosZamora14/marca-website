15850f058752c0667a839f26dd1e2b9a68f21539
Carlos Zamora <carlos14zamora@gmail.com>
2023-09-14 20:55:03 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Marca website
+Clone of the front-end website "marca.com" using the bootstrap library and custom css. The goal was to improve the responsive design while keeping the essense of the page.
--- main.js
+++ main.js
@@ -1,47 +1,49 @@
-// Carousel
-const carousel = document.querySelector('.custom-carousel');
-const leftBtn = document.querySelector('.custom-carousel__left-btn');
-const rightBtn = document.querySelector('.custom-carousel__right-btn');
+(() => {
+  // Carousel
+  const carousel = document.querySelector('.custom-carousel');
+  const leftBtn = document.querySelector('.custom-carousel__left-btn');
+  const rightBtn = document.querySelector('.custom-carousel__right-btn');
 
-function updateControls() {
-  carousel.scrollLeft === 0 ? leftBtn.classList.add('d-none') : leftBtn.classList.remove('d-none');
-  Math.ceil(carousel.scrollLeft + carousel.clientWidth) >= carousel.scrollWidth ? rightBtn.classList.add('d-none') : rightBtn.classList.remove('d-none');
-}
+  function updateControls() {
+    carousel.scrollLeft === 0 ? leftBtn.classList.add('d-none') : leftBtn.classList.remove('d-none');
+    Math.ceil(carousel.scrollLeft + carousel.clientWidth + 0.5) >= carousel.scrollWidth ? rightBtn.classList.add('d-none') : rightBtn.classList.remove('d-none');
+  }
 
-window.addEventListener('resize', () => {
-  updateControls();
-});
+  window.addEventListener('resize', () => {
+    updateControls();
+  });
 
-carousel.addEventListener('scroll', () => {
-  updateControls();
-});
+  carousel.addEventListener('scroll', () => {
+    updateControls();
+  });
 
-leftBtn.addEventListener('click', () => {
-  carousel.scrollLeft -= carousel.clientWidth;
-  updateControls();
-});
+  leftBtn.addEventListener('click', () => {
+    carousel.scrollLeft -= carousel.clientWidth;
+    updateControls();
+  });
 
-rightBtn.addEventListener('click', () => {
-  carousel.scrollLeft += carousel.clientWidth;
-  updateControls();
-});
+  rightBtn.addEventListener('click', () => {
+    carousel.scrollLeft += carousel.clientWidth;
+    updateControls();
+  });
 
-// 'Lo más de Marca' section
-const mostPopularNewsButton = document.querySelector('.lo-mas-news__popular');
-const latestNewsButton = document.querySelector('.lo-mas-news__latest');
-const mostPopularNews = document.querySelector('.lo-mas-news__popular-news');
-const latestNews = document.querySelector('.lo-mas-news__latest-news');
+  // 'Lo más de Marca' section
+  const mostPopularNewsButton = document.querySelector('.lo-mas-news__popular');
+  const latestNewsButton = document.querySelector('.lo-mas-news__latest');
+  const mostPopularNews = document.querySelector('.lo-mas-news__popular-news');
+  const latestNews = document.querySelector('.lo-mas-news__latest-news');
 
-mostPopularNewsButton.addEventListener('click', e => {
-  mostPopularNewsButton.classList.add('active');
-  mostPopularNews.classList.remove('d-none');
-  latestNewsButton.classList.remove('active');
-  latestNews.classList.add('d-none');
-});
+  mostPopularNewsButton.addEventListener('click', e => {
+    mostPopularNewsButton.classList.add('active');
+    mostPopularNews.classList.remove('d-none');
+    latestNewsButton.classList.remove('active');
+    latestNews.classList.add('d-none');
+  });
 
-latestNewsButton.addEventListener('click', e => {
-  latestNewsButton.classList.add('active');
-  latestNews.classList.remove('d-none');
-  mostPopularNewsButton.classList.remove('active');
-  mostPopularNews.classList.add('d-none');
-});
+  latestNewsButton.addEventListener('click', e => {
+    latestNewsButton.classList.add('active');
+    latestNews.classList.remove('d-none');
+    mostPopularNewsButton.classList.remove('active');
+    mostPopularNews.classList.add('d-none');
+  });
+})();
