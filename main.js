46e5a238b0393b09e97e8b0a6def2296ebf3e516
Carlos Zamora <carlos14zamora@gmail.com>
2023-09-14 18:37:44 -0600

    add another news section

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 // Carousel
 const carousel = document.querySelector('.custom-carousel');
-const carouselWidth = carousel.scrollWidth;
 const leftBtn = document.querySelector('.custom-carousel__left-btn');
 const rightBtn = document.querySelector('.custom-carousel__right-btn');
-const link = document.querySelector('.custom-carousel__show-more');
 
-function updateControls(currScrollLeft) {
-  currScrollLeft === 0 ? leftBtn.classList.add('d-none') : leftBtn.classList.remove('d-none');
-  Math.round(currScrollLeft + carousel.clientWidth) >= carouselWidth ? rightBtn.classList.add('d-none') : rightBtn.classList.remove('d-none');
-}
-
-function clampScrollLeft(scrollLeft) {
-  const currScrollLeft = Math.min(scrollLeft, carouselWidth - carousel.clientWidth);
-  return currScrollLeft;
+function updateControls() {
+  carousel.scrollLeft === 0 ? leftBtn.classList.add('d-none') : leftBtn.classList.remove('d-none');
+  Math.ceil(carousel.scrollLeft + carousel.clientWidth) >= carousel.scrollWidth ? rightBtn.classList.add('d-none') : rightBtn.classList.remove('d-none');
 }
 
 window.addEventListener('resize', () => {
-  const currScrollLeft = clampScrollLeft(carousel.scrollLeft);
-  updateControls(currScrollLeft);
+  updateControls();
 });
 
 carousel.addEventListener('scroll', () => {
-  const currScrollLeft = clampScrollLeft(carousel.scrollLeft);
-  updateControls(currScrollLeft);
+  updateControls();
 });
 
 leftBtn.addEventListener('click', () => {
-  const currScrollLeft = Math.max(carousel.scrollLeft - carousel.clientWidth, 0);
-  carousel.scrollLeft = currScrollLeft;
-  updateControls(currScrollLeft);
+  carousel.scrollLeft -= carousel.clientWidth;
+  updateControls();
 });
 
 rightBtn.addEventListener('click', () => {
-  const currScrollLeft = clampScrollLeft(carousel.scrollLeft + carousel.clientWidth);
-  carousel.scrollLeft = currScrollLeft;
-  updateControls(currScrollLeft);
+  carousel.scrollLeft += carousel.clientWidth;
+  updateControls();
+});
+
+// 'Lo más de Marca' section
+const mostPopularNewsButton = document.querySelector('.lo-mas-news__popular');
+const latestNewsButton = document.querySelector('.lo-mas-news__latest');
+const mostPopularNews = document.querySelector('.lo-mas-news__popular-news');
+const latestNews = document.querySelector('.lo-mas-news__latest-news');
+
+mostPopularNewsButton.addEventListener('click', e => {
+  mostPopularNewsButton.classList.add('active');
+  mostPopularNews.classList.remove('d-none');
+  latestNewsButton.classList.remove('active');
+  latestNews.classList.add('d-none');
+});
+
+latestNewsButton.addEventListener('click', e => {
+  latestNewsButton.classList.add('active');
+  latestNews.classList.remove('d-none');
+  mostPopularNewsButton.classList.remove('active');
+  mostPopularNews.classList.add('d-none');
 });
